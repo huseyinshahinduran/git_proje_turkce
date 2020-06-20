a6351386d7583eaf01ec0da318a7d290dba787bb
Huseyn <huseyinsahinduran@gmail.com>
2020-06-20 20:42:02 +0100

    son conflict bugun icin

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
 	System.out.println("first commit from local repository");
 	System.out.println("second commit from local repository");	
 
-	System.out.println("ucuncu commitimizde local reprositoryden");	
-
-	System.out.println("conflict yapmak istiyoruz");	
-
+	
+	
+	System.out.println("2. kez conflict yapiyoruz");
+	
 	System.out.println("first commit from remote repository");
 	
 	
